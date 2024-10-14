b0312875cef2f066e4aa55a3ad87dc0f264422f8
Jae586 <87919104+Jae586@users.noreply.github.com>
2024-10-14 13:15:24 -0700

    Update playnext.test.js

--- .github/workflows/playnext.test.js
+++ .github/workflows/playnext.test.js
@@ -1,33 +1,40 @@
 global.$ = require('jquery');
 
 let currTrack, currPlaylist, getBranch, branchArray;
-let playlistRows; 
+let playlistRows;
 
 beforeEach(() => {
   currTrack = 0;
-  currPlaylist = 1;
-  playlistRows = [ 
-    [
-      ["url1", "FileName1", "Speaker1", 0, "Theme1", "Description1", "TrackName1", "Date1"],
-      ["url2", "FileName2", "Speaker2", 1, "Theme1", "Description2", "TrackName2", "Date2"]
-    ],
-    [
-      ["url3", "FileName3", "Speaker3", 0, "Theme2", "Description3", "TrackName3", "Date3"]
-    ]
-  ];
-  
-  document.body.innerHTML = `
-    <input type="checkbox" id="autoplay" checked />
-    <div class="screen_home"></div>
-    <div class="screen_data"></div>
-  `;
-
+  currPlaylist = 0;
   getBranch = {};
   branchArray = [];
+
+  document.body.innerHTML = `
+    <input type="checkbox" id="autoplay" checked />
+    <div id="playlistContent"></div>
+    <div id="playlistDropdown" class="dropdown-menu"></div>
+    <div id="loader" style="display: none;"></div>
+    <table class="table"></table>
+  `;
+
+  playlistRows = [[], []];
+
+  const values = [
+    ["url1", "FileName1", "Speaker1", 0, "Theme1", "Description1", "TrackName1", "Date1"],
+    ["url2", "FileName2", "Speaker2", 1, "Theme1", "Description2", "TrackName2", "Date2"],
+    ["url3", "FileName3", "Speaker3", 0, "Theme2", "Description3", "TrackName3", "Date3"]
+  ];
+
+  values.forEach((value, i) => {
+    const table = document.querySelector(".table");
+    addRow(table, values, i, playlistRows[currPlaylist], i, currPlaylist, false, []);
+  });
 
   global.audioAt = jest.fn((playlistIndex, trackIndex) => ({
     currentTime: 0,
     play: jest.fn(),
+    paused: false,
+    pause: jest.fn(),
   }));
 
   global.switchPlaylist = jest.fn();
@@ -48,7 +55,7 @@ test('plays next track regardless of autoplay when override is true', () => {
 });
 
 test('switches to next playlist and plays first track', () => {
-  currTrack = 2; 
+  currTrack = 2;
   currPlaylist = 0; 
   playNext(false); 
   expect(switchPlaylist).toHaveBeenCalledWith(1); 
@@ -57,12 +64,13 @@ test('switches to next playlist and plays first track', () => {
 });
 
 test('does not fail when at the end of the playlist', () => {
-  currTrack = 2; // Last track in the first playlist
-  currPlaylist = 0; // First playlist
-  playNext(false); // Should switch to next playlist
+  currTrack = 2; 
+  currPlaylist = 0; 
+  playNext(false); 
   expect(switchPlaylist).toHaveBeenCalledWith(1);
   expect(audioAt).toHaveBeenCalledWith(1, 0);
 });
+
 
 
 
