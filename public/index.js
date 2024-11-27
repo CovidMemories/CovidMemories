bd786cabf3020b055f400cd88e9f126f2f7e0207
daydreamsamurai <daydreamsamurai@gmail.com>
2024-11-27 12:15:14 -0800

    reworking with all the changes
    got a popup working now, next step is tying it to the backend with the forms that i was working on in another branch

--- public/index.js
+++ public/index.js
@@ -314,7 +314,28 @@ export async function deleteHandler(playlistName, playlistOrder) {
 	// reset makes the deleted row disappear (refreshes db)
 	reset();
 }
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+});
 // User attempts to login
 // TODO: use <input> so that user's password appears as
 // * instead of actual thing
